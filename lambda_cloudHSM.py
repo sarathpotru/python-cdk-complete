c023247ee12efc0dfa077e465a543b90c9a63127
Sarath Potru <sarath.potru@stationcasinos.com>
2023-04-03 14:30:40 -0500

    second commit

--- lambda_cloudHSM.py
+++ lambda_cloudHSM.py
@@ -2,69 +2,29 @@ import boto3
 
 def lambda_handler(event, context):
 
-    # Create a CloudHSM client
     client = boto3.client('cloudhsmv2')
 
-    # Define the identifier for the cluster
-    cluster_id = 'my-cluster'
-
-    # Check if a cluster with the same identifier already exists
-    existing_clusters = client.describe_clusters(
-        Filters=[
-            {
-                'Name': 'cluster-id',
-                'Values': [cluster_id]
-            }
-        ]
-    )['Clusters']
-    
-    # If a cluster with the same identifier already exists, do not create a new cluster
-    if existing_clusters:
-        message = f"A cluster with the identifier '{cluster_id}' already exists."
-        return {
-            'statusCode': 400,
-            'body': message
-        }
-    
-    # Define the HSMs for the cluster
-    hsm_type = 'hsm1.medium'
-    hsm_count = 2
-    
-    # Define the subnet IDs for the HSMs
-    subnet_ids = ['subnet-0123456789abcdef0', 'subnet-0123456789abcdef1']
-    
-    # Define the security group IDs for the HSMs
-    security_group_ids = ['sg-0123456789abcdef0', 'sg-0123456789abcdef1']
-    
-    # Define the backup retention period in days
-    backup_retention_period = 7
-    
-    # Define the AZ ID for the cluster
-    availability_zone = 'us-west-2a'
-    
-    # Create the CloudHSM cluster
     response = client.create_cluster(
-        HsmType=hsm_type,
-        SourceBackupId=None,
-        SubnetIds=subnet_ids,
+        HsmType='hsm1.medium',
         BackupRetentionPolicy={
-            'Type': 'DAYS',
-            'Value': backup_retention_period
+        'Type': 'DAYS',
+        'Value': '7'
         },
+        SubnetIds=[
+        'subnet-0211a648cd5b639b2',
+        'subnet-0388ccfadab0b09fe'
+        ],
         TagList=[
             {
                 'Key': 'Name',
-                'Value': cluster_id
+                'Value': 'MyCluster'
             }
-        ],
-        HsmCount=hsm_count,
-        IamRoleArn='arn:aws:iam::123456789012:role/cloudhsmv2-role',
-        VpcId='vpc-0123456789abcdef0',
-        SourceClusterId=None,
-        AvailabilityZone=availability_zone,
-        ClusterId=cluster_id,
-        SecurityGroupIds=security_group_ids
+        ]
     )
+
+    print(response)
     
-    # Return the response
-    return response
+    return {
+        'statusCode': 200,
+        'body': 'Cluster created successfully'
+    }
